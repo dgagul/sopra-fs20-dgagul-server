9d4b888eefe96d57cd906e0d001b917ac55a4c44
dgagul <dario.gagulic@gmail.com>
2020-03-02 20:50:21 +0100

    more info in UserGetDTO now, DTO Mapper adjusted

--- src/main/java/ch/uzh/ifi/seal/soprafs20/rest/dto/UserGetDTO.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs20/rest/dto/UserGetDTO.java
@@ -8,37 +8,44 @@ public class UserGetDTO {
     private String username;
     private String password;
     private UserStatus status;
-
+    private String token;
+    private String creationDate;
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String name) {
-        this.password = name;
     }
 
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
 
+    public String getPassword() { return password; }
+    public void setPassword(String name) { this.password = name; }
+
     public UserStatus getStatus() {
         return status;
     }
-
     public void setStatus(UserStatus status) {
         this.status = status;
+    }
+
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
     }
 }
